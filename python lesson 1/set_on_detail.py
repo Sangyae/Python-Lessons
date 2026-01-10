e489710b18a08c31e6b3564f4c61d0bf8fb714e2
Sangyae <bibekplus100@gmail.com>
2026-01-10 09:49:46 +0000

    second step to set.

---
+++ python lesson 1/set_on_detail.py
@@ -0,0 +1,23 @@
+empty_set = set()
+print(empty_set)
+
+fruits = {'mango', 'banana', 'apple', 'orange', 'banana'}
+print(fruits)
+
+color_list = {'blue', 'red', 'black'}
+print(set(color_list))
+
+if 'blue' in color_list:
+    print("Yes there is blue in color_set.")
+else:
+    print("No there is no colour blue.")
+
+set1 ={1,2,3,4}
+set2 ={4,6,7}
+
+result = any(num in set1 for num in set2)
+
+if result:
+    print("YEs there is at least one common number.")
+else:
+    print("NO there is not even a single common numberes.")
