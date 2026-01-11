6b3f45ab28bf5c8d1ee197a33986e3d6f5e819f2
Sangyae <bibekplus100@gmail.com>
2026-01-11 07:38:21 +0000

    checking if book is available or not in bool.

--- python lesson 1/challenge.py
+++ python lesson 1/challenge.py
@@ -7,7 +7,17 @@ class Library:
         return self.available_books
     
     def is_book_available(self, book):
-        if self.available_books:
-            return True
-        else:
-            return False
+        return book in self.available_books
+        # if self.available_books:
+        #     return True
+        # else:
+        #     return False
+        
+my_book_collection = {'The power of Money', 'Psychology of money', 'The Great gutsby'}
+
+my_library = Library(my_book_collection)
+
+print("My books:",my_library.get_available_books())
+
+print("Is 'The power of Money' available?", my_library.is_book_available('The power of Money'))
+print("Is '1920' available?", my_library.is_book_available('1920'))
