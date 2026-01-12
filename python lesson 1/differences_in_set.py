8f2418045d9c841541b8ddd4a18d6e4a09abc32b
Sangyae <bibekplus100@gmail.com>
2026-01-12 09:55:13 +0000

    difference and symetric difference in set

---
+++ python lesson 1/differences_in_set.py
@@ -0,0 +1,11 @@
+platform1 = {1001,1002,1003,1004,1005}
+platform2 = {1001,1003,1006,1007,1008}
+
+difference_result = platform1 - platform2
+print(difference_result)
+
+difference_result2 = platform2 - platform1
+print(difference_result2)
+
+symetric_difference = platform1 ^ platform2
+print(symetric_difference)
