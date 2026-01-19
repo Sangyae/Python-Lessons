36a6580de7f8ad094670c772ef1eaac4bded5716
Sangyae <bibekplus100@gmail.com>
2026-01-19 21:32:20 +0000

    using f string and adding strings

---
+++ Pythonlesson2/stringFormatting.py
@@ -0,0 +1,4 @@
+str1 = "my number is:"
+str2 = '5'
+print(str1 + str2)
+print(f'my number is:' + str(8))
---
+++ Pythonlesson2/stringSlicing.py
@@ -0,0 +1,5 @@
+name = "My name is Bibek Tamang"
+print(name[0])
+print(name[0:7])
+print(name[:7])         #this prints 0 to 7
+print(name[11:])        #this one starts from 11 to end 
