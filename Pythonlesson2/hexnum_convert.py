4abe46bc4ee3309d4417ec7b8751e27f2aeca58d
Sangyae <bibekplus100@gmail.com>
2026-08-02 09:55:48 +0100

    simple logic to convert hex ot dec but we can write our own function

--- Pythonlesson2/hexnum_convert.py
+++ Pythonlesson2/hexnum_convert.py
@@ -3,5 +3,18 @@ hexNumbers = {
     'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15
 }
 
-def hextoDec(hexnum):
-    return
+def HextoDec(hexNum):
+    for char in hexNum:
+        if char not in hexNum:
+            return "number is not in Hexnumbers"
+
+    if len(hexNum) == 3:
+        return hexNumbers[hexNum[0]] * 256 + hexNumbers[hexNum[1]] * 16 + hexNumbers[hexNum[2]] * 1
+
+    if len(hexNum) == 2:
+        return hexNumbers[hexNum[0]] * 16 + hexNumbers[hexNum[1]] * 1
+
+    if len(hexNum) == 1:
+        return hexNumbers[hexNum[0]] 
+
+
