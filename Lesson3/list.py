680e2d7ea011ebecdc044b9c30b37af179b1a879
Sangyae <bibekplus100@gmail.com>
2026-08-06 19:16:10 +0100

    list and chopping list i mean playing with lists

---
+++ Lesson3/list.py
@@ -0,0 +1,20 @@
+# Mylist = ['1','2', '3']
+# print(Mylist)
+
+# Mylist.append(0) #append adds the value at last
+# print(Mylist)
+
+# Mylist.insert(2, '5') # insert allows to insert any value in any index
+# print(Mylist)
+
+# Mylist.remove('5') # does not take index to remove but the value
+# print(Mylist)
+
+# for i in range(10):
+#     print(i)
+
+Mylist = list(range(10))
+print(Mylist)
+
+print(Mylist[:2])
+print(Mylist[::3])
